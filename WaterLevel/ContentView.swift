78d3cd8940756a100582ecb9083c58856eee42e0
Matt Mangum <mtmangum@utexas.edu>
2026-08-07 21:28:37 -0500

    Initial Commit

---
+++ WaterLevel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WaterLevel
+//
+//  Created by bollox on 8/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaterLevel/WaterLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterLevelApp.swift
+//  WaterLevel
+//
+//  Created by bollox on 8/7/26.
+//
+
+import SwiftUI
+
+@main
+struct WaterLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
